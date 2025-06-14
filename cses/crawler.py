95fd32c4af44f1a978f90b9b97d1f89aebd26824
Yash12930 <yashsc2005@gmail.com>
2025-06-14 20:42:32 +0530

    download files

---
+++ cses/crawler.py
@@ -0,0 +1,171 @@
+from urllib.parse import urljoin
+import requests
+from bs4 import BeautifulSoup as bs
+import os
+from htmlgen import *
+from collections import defaultdict
+import sys
+
+url='https://cses.fi/problemset'
+cookies={}
+OUTPUT_DIR = "C:/Users/yashs/c c++/cpp/leetcodesolve/cses"
+
+def download_url(s,url):
+    return requests.get(url,cookies=cookies).text
+
+def login(s):
+    print('Username: ',end='')
+    username=input()
+    print('Password: ',end='')
+    password=input()
+    url='https://cses.fi/login'
+    payload={'nick':username,
+            'pass':password,
+            }
+    p=s.get(url)
+    cookies['PHPSESSID']=p.cookies['PHPSESSID']
+    html=p.text
+    soup=bs(html,'html.parser')
+    payload['csrf_token']=soup.find('input',{'name':'csrf_token'})['value']
+    p=s.post(url,data=payload)
+    soup=bs(p.text,'html.parser')
+    if soup.find('a',{"class":"account"})["href"]=="/login":
+        print('Log in failed!')
+        exit(0)
+    print('Logged in successfully')
+
+class Problem:
+    def __init__(self,s,probUrl):
+        html=download_url(s,probUrl) 
+        self.url=probUrl
+        self.soup = bs(html, 'html.parser')
+        self.aclink = None
+        self.actime = None
+        self.accode= None
+        self.accodehtml = None
+        self.accodelang = None
+        self.title = self.soup.h1.getText()
+        self.cate = self.soup.h4.getText()
+        print('- Fetching: %s > %s'%(self.cate,self.title))
+        for link in self.soup.find_all('a'):
+            path = link.get('href')
+            if path and path.startswith('/problemset/result'):
+                path = urljoin(url, path)
+                if not link.span: continue
+                if 'full' in link.span.get('class'):
+                    self.aclink=path
+                    self.getSubmission(s,path)
+                    print(' -> Status: Solved / %s / %s'%(self.accodelang,self.actime))
+                    return
+        print(' -> Status: Not Solved')
+        
+    def getSubmission(self,s,resultUrl):
+        html=download_url(s,resultUrl)
+        self.soupac=bs(html,'html.parser')
+        self.actime=self.soupac.table.find_all('tr')[2].find_all('td')[1].getText()
+        lang=self.soupac.table.find_all('tr')[3].find_all('td')[1].getText()
+        if 'C++' in lang:
+            self.accodelang='cpp'
+        elif 'Py' in lang:
+            self.accodelang='python'
+
+        # Code section
+        tmp=self.soupac.find('pre',{'class':'linenums'})
+        self.accodehtml=tmp.string.wrap(self.soupac.new_tag('code'))
+        self.accodehtml['class']="language-%s"%self.accodelang
+        self.accode=tmp.getText()
+
+def getProblems(s,urls):
+    for url in urls:
+        yield(Problem(s,url))
+
+def writeToFile(filename,content):
+    full_path = os.path.join(OUTPUT_DIR, filename)
+    os.makedirs(os.path.dirname(full_path), exist_ok=True)
+    with open(full_path, 'w', encoding='utf-8') as f:
+        f.write(content)
+
+def writeCodeToDirectory(problem):
+    if problem.accodelang==None: return
+    ext='unknown'
+    if problem.accodelang=='cpp':
+        ext='cpp'
+    elif problem.accodelang=='python':
+        ext='py'
+    filename='%s/%s.%s'%(problem.cate,problem.title,ext)
+    writeToFile(filename,problem.accode)
+
+def main(argv):
+    with requests.Session() as s:
+        html=download_url(s,url)
+        soup=bs(html,'html.parser')
+        problemUrls=[]
+        for link in soup.find_all('a'):
+            path = link.get('href')
+            if path and path.startswith('/problemset/task'):
+                problemUrls.append(urljoin(url, path))
+
+        def asking():
+            # ask if want to download solution
+            solFlag=False
+            print("Do you want to download solution code (y/N)? ",end="")
+            if input().strip().lower()=='y': solFlag=True
+
+            # ask if want to package the solutions to an web interface (open index.html to view)
+            packageFlag=False
+            print("Do you want to package solution code to web interface (y/N)? ",end="")
+            if input().strip().lower()=='y': packageFlag=True
+            print("-------------------------------------------------------------")
+            return (solFlag,packageFlag)
+
+
+        # no args
+        if len(argv)==1:
+            login(s)
+            solFlag,packageFlag=asking()
+            sidebar=defaultdict(list)
+            cnt=0
+            for problem in getProblems(s,problemUrls):
+                if not problem.accodelang: 
+                    continue
+                sidebar[problem.cate].append(problem.title)
+                
+                filename='%s/%s.html'%(problem.cate,problem.title)
+                if solFlag:
+                    writeCodeToDirectory(problem)
+                if packageFlag:
+                    writeToFile(filename,genHtmlProblem((problem.title,problem.url,str(problem.accodehtml))))
+                cnt+=1
+            if solFlag:
+                print('-> Save code successfully')
+            if packageFlag:
+                writeToFile("./index.html",genHtmlIndex(sidebar))
+                print('-> Generated index.html successfully')
+            print("You solved %d problems"%cnt)
+
+        # args = [problemID]
+        elif len(argv)==2:
+            login(s)
+            probUrl='https://cses.fi/problemset/task/%s'%argv[1]
+            if not probUrl in problemUrls:
+                print("ProblemID not found")
+                return
+            solFlag,packageFlag=asking()
+            problem=Problem(s,probUrl)
+            if solFlag:
+                writeCodeToDirectory(problem)
+                print('-> Save code successfully')
+            if packageFlag:
+                writeToFile(filename,genHtmlProblem((problem.title,problem.url,str(problem.accodehtml))))
+                print('-> Save html code successfully')
+        else: printUsage()
+
+def printUsage():
+    print("""Usage:
+    `python crawler.py` to download all problems
+    `python crawler.py <problemID>` to download a specific problem
+""")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ cses/htmlgen.py
@@ -0,0 +1,144 @@
+def genHtmlProblem(param):return """
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.26.0/themes/prism-tomorrow.min.css" integrity="sha512-vswe+cgvic/XBoF1OcM/TeJ2FW0OofqAVdCZiEYkd6dwGXthvkSFWOoGGJgS2CW70VK5dQM5Oh+7ne47s74VTg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.26.0/plugins/line-numbers/prism-line-numbers.min.css" integrity="sha512-cbQXwDFK7lj2Fqfkuxbo5iD1dSbLlJGXGpfTDqbggqjHJeyzx88I3rfwjS38WJag/ihH7lzuGlGHpDBymLirZQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cabin" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="../styles.css">
+<link rel="stylesheet" type="text/css" href="../content.css">
+
+<body>
+<div class="card">
+	<h2>Problem: %s</h2>
+	<br>
+	<div>
+		<b>Solution Contributor:</b> <a target="_blank" href="https://github.com/DecSP">Cao Son Nguyen</a>.<br>
+		<a target="_blank" href="%s">View on CSES</a>
+	</div>
+
+	
+	<h3>Code</h3>
+	<ul>
+		<pre class="line-numbers">%s</pre>
+	</ul>
+</div>
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.26.0/components/prism-core.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.26.0/plugins/autoloader/prism-autoloader.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.26.0/plugins/line-numbers/prism-line-numbers.min.js" integrity="sha512-dubtf8xMHSQlExGRQ5R7toxHLgSDZ0K7AunqPWHXmJQ8XyVIG19S1T95gBxlAeGOK02P4Da2RTnQz0Za0H0ebQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+
+"""%param
+def genHtmlIndex(sidebar):
+	re=""
+	for cate in sidebar:
+		tmp="<summary>%s</summary>"%cate
+		for title in sidebar[cate]:
+			tmp+="""<li><a href="Javascript:void(0);" id="about" onclick="loadHtml('%s/%s.html')">%s</a></li>\n"""%(cate,title,title)
+		re+="<details>%s</details>"%tmp
+	return """
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CSES Sols</title>
+	<meta name="theme-color" content="#57CC99">
+	<meta property="og:image" content="public/og.png"/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:url" content="https://csessolutions.netlify.app/"/>
+	<meta property="og:title" content="CSES Solutions"/>
+	<meta property="og:description" content="We provide our solutions for coding problems of CSES site that is owned by Antti Laaksonen & Topi Talvitie during our data structures and algorithms learning. Most of the solutions are written in C++ and Python programming language."/>
+	<link rel="icon" href="public/icon.png">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Cabin" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Fugaz+One" rel="stylesheet">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+
+	<script>
+	var sideCate='';
+	var sideProb='';
+	function highlightSide(){
+		const cates=document.getElementsByTagName('summary');
+		for (const cate of cates){
+			if (cate.innerHTML==sideCate){
+				cate.classList.toggle('highlight')
+				for (const prob of cate.parentElement.children){
+					if (prob.firstChild.innerHTML==sideProb){
+						prob.classList.toggle('highlight')
+						break;
+					}
+				}
+				break;
+			}
+		}
+	}
+	function loadHtml(filename){
+		document.getElementById('content').innerHTML = '<iframe class="iframe" frameborder="0" src="'+filename+'">';
+
+		const collection=filename.split('/');
+		highlightSide();
+		if (collection.length==2){
+			sideCate=collection[0]
+			sideProb=collection[1].substring(0,collection[1].length-5)
+			highlightSide();
+			document.title=`${sideProb} | CSES Sols`;
+		}
+		else{
+			sideCate=''
+			sideProb=''
+			document.title=`CSES Sols`;	
+		}
+	}
+	function clearNav(){
+		document.getElementById('home').style.textDecoration="none";
+		document.getElementById('about').style.textDecoration="none";
+	}
+	function setNav(id){
+		if(id === 'home'){
+			document.getElementById('home').style.textDecoration="underline";
+		}
+		if(id === 'contact'){
+			document.getElementById('about').style.textDecoration="underline";
+		}
+	}
+	var open = 0;
+	if(screen.width <= 670){
+		document.getElementById('sidebar').style.display="none";
+	}
+	function showSidebar(){
+		open++;
+		if(open%2 ==1){
+			document.getElementById('sidebar').style.display="block";
+		}
+		else if(open%2 == 0){
+			document.getElementById('sidebar').style.display="none";
+		}
+	}
+	</script>
+</head>
+<body>
+	<div class="header">
+		<a href="Javascript:void(0);"><span>CSES Algo...</span></a>
+		<div class="navbar">
+			<a href="Javascript:void(0);" id="about" onclick="loadHtml('about.html')">About us</a>
+
+		</div>
+	</div>
+	<div id="main">
+		<div class="sidebar" id="sidebar">
+			%s
+		</div>
+		<div class="content" id="content">
+
+		</div>
+	</div>
+</body>
+
+</html>
+"""%re
